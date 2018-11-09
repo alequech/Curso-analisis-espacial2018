db222ee06f1ccd7b4e02ba5d9ab7c071379f1605
“Alexander <alequech@gmail.com>
2018-11-08 23:21:18 -0600

    Tic's

---
+++ 1_Datos_Vectoriales/InstalaPaquetes.R
@@ -0,0 +1,21 @@
+rm(list=ls(all=TRUE))
+
+packages<-c("sf","sp","rgdal","rgl","rgeos","maptools","animation","igraph",
+	"plyr","msm","png","fields","tiff","gstat","lattice","latticeExtra","gridExtra",
+	"ggplot2","snow","maps","fBasics","data.table","raster","rasterVis","knitr",
+	"htmltools","caTools","bitops","mapview","jpeg","gWidgets","leaflet","spData","tidyverse","caret")
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# usage
+ipak(packages)
+install.packages("spDataLarge", type = "source")
+
+###############################
+#########END OF SCRIPT#########
+###############################
